d47bc80c2069792b2105574d3f13a45c1797b8b7
Alex Abramov <rboots@ya.ru>
2014-12-27 18:54:00 +0000

    refactor command line

--- src/main.js
+++ src/main.js
@@ -4,23 +4,31 @@ var optimist = require('optimist');
 var watch = require('./watch');
 var compile = require('./compile');
 
-var argv = optimist
-	.usage('Preprocessor for JavaScript.\n\nUsage:\njas --watch myfile.jas -o myfile.js\njas --compile myfile.jas -o myfile.js')
-	.alias('w', 'watch')
-	.describe('w', 'File or directory for watching')
-	.alias('c', 'compile')
-	.describe('c', 'File or directory for compiling')
-	.alias('o', 'output')
-	.describe('o', 'Output file or directory')
-	.argv;
+exports.processCommandLine = function () {
+	var argv = optimist
+		.usage('Preprocessor for JavaScript.\n\nUsage:\njas --watch myfile.jas -o myfile.js\njas --compile myfile.jas -o myfile.js')
+		.alias('w', 'watch')
+		.describe('w', 'File or directory for watching')
+		.alias('c', 'compile')
+		.describe('c', 'File or directory for compiling')
+		.alias('o', 'output')
+		.describe('o', 'Output file or directory')
+		.argv;
 
-if (argv.watch) {
-	watch.start(argv.watch, argv.output);
-}
-else if (argv.compile) {
-	compile.start(argv.compile, argv.output);
-}
-else {
-	optimist.showHelp();
-	process.exit(1);
-}
+	if (argv.watch) {
+		watch.start(argv.watch, argv.output);
+	}
+	else if (argv.compile) {
+		compile.start(argv.compile, argv.output);
+	}
+	else {
+		optimist.showHelp();
+		process.exit(1);
+	}
+};
+
+exports.watch = watch.start.bind(watch);
+
+exports.compile = compile.start.bind(compile);
+
+exports.showHelp = optimist.showHelp.bind(optimist);
